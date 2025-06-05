ba5e5ce52e6638bf179928f9e29b5044fa1b8e9e
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-06-05 16:35:44 +0200

    T1 acabat

--- M03-05.Recuperacio.MiquelManzano/M03-05.Recuperacio.MiquelManzano/Program.cs
+++ M03-05.Recuperacio.MiquelManzano/M03-05.Recuperacio.MiquelManzano/Program.cs
@@ -5,8 +5,8 @@
         private static void Main(string[] args)
         {
             const int Min = 0;
-            const int Max = 2;
-            const string MenuEx = "0. Exit\n1. T1 - Activitats: Exercici 7";
+            const int Max = 3;
+            const string MenuEx = "0. Exit\n1. T1 - Activitats: Exercici 7\n2. T1 - Activitats: Exercici 8\n3. T1 - Activitats: Exercici 9";
 
             Console.WriteLine("M03-05.Recuperacio.MiquelManzano");
             Console.WriteLine(MenuEx);
@@ -21,6 +21,10 @@
                     T1Exercises.ShowPairs();
                     break;
                 case 2:
+                    T1Exercises.ShowNumAvg();
+                    break;
+                case 3:
+                    T1Exercises.ExampleExpressions();
                     break;
             }
         }
--- M03-05.Recuperacio.MiquelManzano/M03-05.Recuperacio.MiquelManzano/T1Exercises.cs
+++ M03-05.Recuperacio.MiquelManzano/M03-05.Recuperacio.MiquelManzano/T1Exercises.cs
@@ -8,11 +8,11 @@ namespace M03_05.Recuperacio.MiquelManzano
 {
     public class T1Exercises
     {
+        const int Min = 0;
+        const int Max = 100;
+
         public static void ShowPairs()
         {
-            const int Min = 0;
-            const int Max = 100;
-
             Console.Clear();
             Console.WriteLine("T1 - Activitats: Exercici 7");
 
@@ -27,6 +27,44 @@ namespace M03_05.Recuperacio.MiquelManzano
                     Console.Write($"{i} ");
                 }
             }
+        }
+
+        public static void ShowNumAvg()
+        {
+            const string AskN = "Cuants numeros vols introduir?";
+
+            Console.Clear();
+            Console.WriteLine("T1 - Activitats: Exercici 8");
+
+            Console.WriteLine(AskN);
+            int n = UserInteraction.AskUserIntNumber(Min, Max);
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introdueix el número {i + 1}: ");
+                sum += UserInteraction.AskUserIntNumber(Min, Max);
+            }
+            double avg = (double)sum / n;
+
+            Console.WriteLine($"La mitjana es: {avg}");
+        }
+
+        public static void ExampleExpressions()
+        {
+            Console.Clear();
+            Console.WriteLine("T1 - Activitats: Exercici 9");
+            Console.WriteLine("Prova de finestra Watch");
+
+            int x = 4;
+            int y = 7;
+            bool p = true;
+            bool q = false;
+
+            bool ex1 = !(x * y < 30) && p;
+            bool ex2 = (x - y) <= 5 && !(p || q);
+
+            Console.WriteLine($"Primera expressio: {ex1}\nSegona expressio: {ex2}");
         }
     }
 }
